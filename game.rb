ec34f6c84668da4db226a44b6119efe2115c0362
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-06 14:33:30 +0000

    fundemental functions that define the concept of the game complete. The first part of the game layed out in 'def lower_level' is also complete

--- game.rb
+++ game.rb
@@ -31,7 +31,7 @@ class Game
         process.exit(1)
     end
 
-    def central_corridor 
+    def lower_level 
         puts "Your ship has just landed on a deep space mining colony"
         puts "You were sent there to investigate the facility as mineral production had ceased"
         puts "Your mission is to investigate and return the findings to mothership"
@@ -46,3 +46,39 @@ class Game
 
         prompt()
         action = gets.chomp()
+
+        if action == "take a quick sample"
+            puts "you take out your tools and take a sample using a swab and a petri dish"
+            puts "you can see that the alien pod is unfolding further"
+            puts "an octopus like unidentified lifeform is squirming around inside and attempting to break free"
+            puts "you realise that is is an egg!"
+            puts "it's time to get out of there with the sample"
+            puts "as you turn to leave the lower level you see countless eggs unfolding"
+            puts "You need to get back to to your ship"
+            return :upper_level
+
+        elsif action == "run"
+            puts "you panic"
+            puts "in a desperate attemt to climb up out of the lower level you slip"
+            puts "the fall from the metal ladder breaks both of your legs"
+            puts "you lie amongst the alien life forms and watch an ocotpus like creature immerge"
+            puts "it zag zags toward you as you pray that the alien isn't hostile"
+            puts "the facehugger leaps into the air and onto your face"
+            puts "you feel its tail tighten round your neck, its arms clasp your face and a slimey tube enter your mouth"
+            return :death
+
+        elsif action == "wait and see what unfolds"
+            puts "the scientist within is in awe"
+            puts "you watch as an ocotpus like creature emerges from its egg"
+            puts "the creature moves slowly onto your arm and caresses you"
+            puts "you feel a bond between human and alien"
+            puts "you are the first human to encounter sentient life"
+            puts "the alien leaps onto your face and all turns black"
+            return :death
+        
+        else "DOES NOT COMPUTE. SELECT ONE OF THE THREE OPTIONS"
+            return :lower_level
+        end
+    end
+
+    def upper_level
